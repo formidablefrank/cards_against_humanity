fe2240d9fe2f22f9e7f5fca38c3a88980e21276e
J Franco Ray <kean.franco07@gmail.com>
2026-01-08 17:14:12 +0100

    Add allowed hosts configuration for production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,4 +82,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Add allowed hosts
+  config.hosts << 'www.example.com'
 end
